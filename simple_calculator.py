c1f3dfb005d2b1ee0e55c6cf60428fca05cca7cc
God father <92206055+rffj@users.noreply.github.com>
2021-12-17 22:49:29 +0530

    Hmk
    Hmm

---
+++ simple_calculator.py
@@ -0,0 +1,17 @@
+v = int(input())
+b = input()
+c = int(input())
+
+if (b == '+'):
+	a = v+ c
+	print('your answer is =>',a)
+elif (b == '*'):
+	u = v * c
+	print('your answer is =>',u)
+elif (b == '-'):
+	s = v - c
+	print('your answer is =>',s)
+elif (b == '%'):
+	f = v % c
+	print('your answer is =>',f)
+
